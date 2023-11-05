15e8654174f3d8b8c732042255299bd1ed12ec98
Alexandre Ferreira <alexandre.cicconi@yahoo.fr>
2023-11-05 18:48:39 +0100

    authentication

---
+++ auth.config.ts
@@ -0,0 +1,22 @@
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  providers: [],
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+        const isLoggedIn = !!auth?.user;
+        const isOnDashBoard = nextUrl.pathname.startsWith('/dashboard');
+
+        if (isOnDashBoard) {
+            return isLoggedIn ? true : false;
+        } else if (isLoggedIn) {
+            return Response.redirect(new URL('/dashboard', nextUrl));
+        }
+
+        return true;
+    },
+  }
+} satisfies NextAuthConfig;
